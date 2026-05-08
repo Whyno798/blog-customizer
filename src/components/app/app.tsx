dcebd1b21db9bcaa34272d5bd6320cb3f0a69799
Whyno <79536860663@mail.ru>
2026-05-08 18:18:54 +0300

    after 3 steps

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,63 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from 'src/constants/articleProps';
 
 import styles from './app.module.scss';
 
 export const App = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState);
+	const [appliedState, setAppliedState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	function handleChange<K extends keyof ArticleStateType>(
+		key: K,
+		value: ArticleStateType[K]
+	) {
+		setFormState((prev) => ({
+			...prev,
+			[key]: value,
+		}));
+	}
+
+	const handleApply = () => {
+		setAppliedState(formState);
+		setIsOpen(false);
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		setAppliedState(defaultArticleState);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': appliedState.fontFamilyOption.value,
+					'--font-size': appliedState.fontSizeOption.value,
+					'--font-color': appliedState.fontColor.value,
+					'--container-width': appliedState.contentWidth.value,
+					'--bg-color': appliedState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				isOpen={isOpen}
+				values={formState}
+				onArrowClick={() => setIsOpen((prev) => !prev)}
+				onApply={handleApply}
+				onReset={handleReset}
+				onChange={handleChange}
+			/>
+
 			<Article />
 		</main>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,129 @@
+import clsx from 'clsx';
+import { FormEvent, useEffect, useRef } from 'react';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	isOpen: boolean;
+	values: ArticleStateType;
+	onArrowClick: () => void;
+	onApply: () => void;
+	onReset: () => void;
+	onChange: <K extends keyof ArticleStateType>(
+		key: K,
+		value: ArticleStateType[K]
+	) => void;
+};
+
+export const ArticleParamsForm = ({
+	isOpen,
+	values,
+	onArrowClick,
+	onApply,
+	onReset,
+	onChange,
+}: ArticleParamsFormProps) => {
+	const asideRef = useRef<HTMLElement | null>(null);
+
+	useEffect(() => {
+		if (!isOpen) {
+			return;
+		}
+
+		const handleClickOutside = (event: MouseEvent) => {
+			const target = event.target as Node;
+
+			if (asideRef.current && !asideRef.current.contains(target)) {
+				onArrowClick();
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen, onArrowClick]);
+
+	const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+		evt.preventDefault();
+		onApply();
+	};
+
+	const handleReset = (evt: FormEvent<HTMLFormElement>) => {
+		evt.preventDefault();
+		onReset();
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={onArrowClick} />
+
+			<aside
+				ref={asideRef}
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+
+					<Separator />
+
+					<Select
+						title='Шрифт'
+						selected={values.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) => onChange('fontFamilyOption', option)}
+					/>
+
+					<RadioGroup
+						title='Размер шрифта'
+						name='font-size'
+						options={fontSizeOptions}
+						selected={values.fontSizeOption}
+						onChange={(option) => onChange('fontSizeOption', option)}
+					/>
+
+					<Select
+						title='Цвет шрифта'
+						selected={values.fontColor}
+						options={fontColors}
+						onChange={(option) => onChange('fontColor', option)}
+					/>
+
+					<Select
+						title='Цвет фона'
+						selected={values.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => onChange('backgroundColor', option)}
+					/>
+
+					<Select
+						title='Ширина контента'
+						selected={values.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => onChange('contentWidth', option)}
+					/>
+
+					<Separator />
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
